bb567b1b55d13201575973e84681a380853a25bb
praveenshantiinfosoft <155941021+praveenshantiinfosoft@users.noreply.github.com>
2025-04-30 09:06:49 +0000

    bug fix

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,8 +3,6 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 
-
-
 router.get('/', userController.getAllUsers);
 router.post('/', userController.createUser);
 router.get('/:id', userController.getUserById);
