3f98a23a8a9e901cb3786c2e5830b486410e8d2d
praveenshantiinfosoft <155941021+praveenshantiinfosoft@users.noreply.github.com>
2025-04-30 09:04:07 +0000

    testr22

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,6 +3,8 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 
+
+
 router.get('/', userController.getAllUsers);
 router.post('/', userController.createUser);
 router.get('/:id', userController.getUserById);
